75c2ab3d7b06a53d398fed5ca273f25c74627409
Crackvenger <tsukinome123@gmail.com>
2024-07-22 09:33:13 +0700

    updated UI.py, login page now connects to the main

--- UI.py
+++ UI.py
@@ -1,41 +1,56 @@
 from nicegui import app, ui
 import speech_recognition as sr
 
-recognizer = sr.Recognizer()
-microphone = sr.Microphone()
+# recognizer = sr.Recognizer()
+# microphone = sr.Microphone()
+data = {"alcon": "marmut", 'test': 'pisang'}
 
-
-def get_command():
-    try:
-        with microphone as source:
-            micAudio = recognizer.listen(source)
-            command = recognizer.recognize_google(micAudio)
-    except sr.UnknownValueError:
-        command = "I do not understand"
-    except sr.WaitTimeoutError:
-        command = "No speech detected"
-    except Exception as e:
-        command = "An error occured"
-    return command
-
+# def get_command():
+    # try:
+        # with microphone as source:
+            # micAudio = recognizer.listen(source)
+            # command = recognizer.recognize_google(micAudio)
+    # except sr.UnknownValueError:
+        # command = "I do not understand"
+    # except sr.WaitTimeoutError:
+        # command = "No speech detected"
+    # except Exception as e:
+        # command = "An error occured"
+    # return command
 
 
 @ui.refreshable
 def check_txt(data):
     speechLog = ui.log(max_lines=100).classes('w-full h-60')
     speechLog.push(data)
-            
 
-ui.label("Smart home app")
-  
+@ui.page('/login')
+def login():    
+    def check_pass():
+        if data.get(username.value) == password.value:
+            ui.notify("Correct password!")
+            ui.navigate.to('main_content')
+        else:
+            ui.notify("something is not right")
 
-with ui.row():
-    ui.button(text="turn on light 1")
-    ui.button(text="turn on light 2")
-    ui.button(text="turn on light 3")
-with open('test.txt', 'r+') as file:
-    data = file.read()
-check_txt(data)
-ui.timer(1, lambda: check_txt.refresh(data))
+    with ui.card().classes('absolute-center'):
+        username = ui.input('Username')
+        password = ui.input('Password', password=True, password_toggle_button=True)
+        ui.button('Log in', on_click=check_pass)
 
+@ui.page('/main_content')
+def main_content():
+
+    with ui.card(align_items='center').classes('absolute-center'):
+        ui.label("Smart home app")
+        with ui.row():
+            ui.button(text="turn on light 1")
+            ui.button(text="turn on light 2")
+            ui.button(text="turn on light 3")
+        with open('test.txt', 'r+') as file:
+            data = file.read()
+        check_txt(data)
+        ui.timer(4, lambda: check_txt.refresh(data))
+
+login()
 ui.run()
