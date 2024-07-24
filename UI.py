f33a396608f926200d253fe942baeb55b8f1cc24
Crackvenger <tsukinome123@gmail.com>
2024-07-24 09:40:29 +0700

    problems with niceGUI awaiting page

--- UI.py
+++ UI.py
@@ -22,13 +22,14 @@ data = {"alcon": "marmut", 'test': 'pisang'}
 def check_txt(data):
     speechLog = ui.log(max_lines=100).classes('w-full h-60')
     speechLog.push(data)
+    ui.update(speechLog)
 
 @ui.page('/login')
 def login():    
     def check_pass():
         if data.get(username.value) == password.value:
             ui.notify("Correct password!")
-            ui.navigate.to('await')
+            ui.navigate.to(static_mode)
         else:
             ui.notify("something is not right")
 
@@ -42,19 +43,25 @@ def static_mode():
     ui.label('Awaiting voice activation')
     with open('test.txt', 'r') as file:
         data = file.read()
+    if "ikaris" or "Icarus" in data:
+        ui.navigate.to(main_content)
+        
 @ui.page('/main_content')
 def main_content():
-
-    with ui.card(align_items='center').classes('absolute-center'):
-        ui.label("Smart home app")
-        with ui.row():
-            ui.button(text="turn on light 1")
-            ui.button(text="turn on light 2")
-            ui.button(text="turn on light 3")
-        with open('test.txt', 'r+') as file:
-            data = file.read()
-        check_txt(data)
-        ui.timer(4, lambda: check_txt.refresh(data))
+    with open('test.txt', 'r') as file:
+        data = file.read()
+    if "ikaris" or "Icarus" in data:
+        with ui.card(align_items='center').classes('absolute-center'):
+            ui.label("Smart home app")
+            with ui.row():
+                ui.button(text="turn on light 1")
+                ui.button(text="turn on light 2")
+                ui.button(text="turn on light 3")
+                check_txt(data)
+                ui.timer(4, lambda: check_txt.refresh(data))
+    else:
+        ui.label("Waiting....")
+    
 
 login()
 ui.run()
